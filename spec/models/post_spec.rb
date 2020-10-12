c54b522d0856cf008b9dcd9a50d6b5c23715d15b
Sandra Vilder <sandra.vilder@gmail.com>
2020-10-12 16:20:09 +0200

    tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
   # context "with 2 or more comments" do
   #   it "orders them in reverse chronologically" do
   #     post = Post.create!
@@ -9,4 +13,11 @@ RSpec.describe Post, type: :model do
   #     expect(post.reload.comments).to eq([comment2, comment1])
   #   end
   # end
+
+  # describe 'validations' do
+  #     # Here we're using FactoryBot, but you could use anything
+  #     post = Post.create!
+
+  #     it { should validate_presence_of(:title) }
+  #   end
 end
